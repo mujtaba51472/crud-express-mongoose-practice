957d1bd1b8b5dce778e68f27197217168ea8d955
mujtaba514 <mujtaba51472@gmail.com>
2023-02-14 21:50:18 +0500

    add delete and fetch

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -12,3 +12,37 @@ export const addUser = async (req, res) => {
   }
   console.log("add user controller", user);
 };
+
+export const getUser = async (req, res) => {
+  const user = await userModel.find();
+  console.log("userbck", user);
+  try {
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete user
+
+export const deleteUser = async (req, res) => {
+  console.log("iddd", req.params.id);
+  const result = await userModel.findByIdAndDelete(req.params.id);
+  try {
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// edit
+
+export const editUser = async (req, res) => {
+  console.log("iddd", req.params.id);
+  const result = await userModel.findOneAndUpdate(req.params.id);
+  try {
+    res.status(201).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,8 +1,16 @@
 import express from "express";
 const Router = express.Router();
-import { addUser } from "../controllers/user-controller.js";
+import {
+  addUser,
+  getUser,
+  deleteUser,
+  editUser,
+} from "../controllers/user-controller.js";
 
 // routes
 Router.post("/add", addUser);
+Router.get("/alluser", getUser);
+Router.delete("/delete/:id", deleteUser);
+Router.put("/edit/:id", editUser);
 
 export default Router;
